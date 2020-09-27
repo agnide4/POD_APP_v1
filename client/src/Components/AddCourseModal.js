af3ae73787bcde340b3dd7fc6a61dbd87b9827cd
Samir Bello <agnide4@gmail.com>
2020-09-27 12:59:30 -0500

    Add course still not working

--- client/src/Components/AddCourseModal.js
+++ client/src/Components/AddCourseModal.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useEffect } from "react"
+import { useState, useEffect, useReducer } from "react"
 import { addCourses } from "../actions"
 import { Form, Modal, Button, InputGroup } from 'react-bootstrap';
 import { useDispatch, useSelector } from "react-redux";
@@ -13,20 +13,40 @@ export default function AddCourseModal(props) {
     ] 
     )
     const [validated, setValidated] = useState(false);
-    const [checked, setChecked] = useState(false)
+    const [newCourse, setNewCourse] = useReducer(
+        (state, newState) => ({...state, ...newState}),
+    {
+        course: '',
+        subject: '',
+       
+    }
+    )
 
     const handleChange = (e) =>{
-        Course Name : 
-        
+        const target = e.target;
+        const name = target.name
+        const value = target.value
+        setNewCourse({[name]: value} )
     }
+    const handleChange1 = (e) =>{
+        const target = e.target;
+        const name = target.name
+        const value = target.value
+        setNewCourse({[name]: value} )
+    }
+
+
 
     const handleSubmit = (e) =>{
         const form = e.currentTarget;
+        e.preventDefault();
+        e.stopPropagation();
         if (form.checkValidity() === false) {
             e.preventDefault();
             e.stopPropagation();
         }
         setValidated(true);
+        console.log(newCourse)
 
     }
   
@@ -42,21 +62,21 @@ export default function AddCourseModal(props) {
                <Modal.Title>New Course</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form noValidate validated={validated} onSubmit={ handleSubmit }>
+            <Form noValidate validated={validated} onSubmit={ handleSubmit }>
                 <Form.Group controlId="formPlainText">
                     <Form.Label>Course Name</Form.Label>
-                    <Form.Control  type="input" placeholder="Add New Course" required/>
+                    <Form.Control  value={newCourse.course} placeholder="Add New Course" name="course" required onChange={handleChange}/>
                     <Form.Control.Feedback type="invalid">New course Name required</Form.Control.Feedback>  
                 </Form.Group>
-                <Form.Group controlId="Subject" required onSelectCapture>
-                    <Form.Control as="select">
+                <Form.Group controlId="subject"  name="subject" required value={newCourse.subject} onSelect={handleChange} >
+                    <Form.Control as="dropdown" type="input" >
+                        <option></option>
                         <option>Mathematics</option>
                         <option>Science</option>
                         <option>Social studies</option>
                         <option>Language Arts</option>
                         <option>Computer Science</option>
-                    </Form.Control>
-                      
+                    </Form.Control>        
                 </Form.Group>
                 <Button className="primary-button" type="submit">Submit</Button>
             </Form>   
